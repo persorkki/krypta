ece5e12034fcb6362a283fa5de4ed46f1fdcf60a
persorkki <persorkki@protonmail.com>
2024-01-28 01:31:49 +0200

    more visual updates, dashboard layout and upload page pretty much done

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,14 +1,46 @@
 <script>
 	import '../../app.css';
-	import Side from '$lib/components/Side.svelte';
+	import { slide } from 'svelte/transition';
+	import { page } from '$app/stores';
 </script>
 
-<div class="flex">
-	<!-- sidebar -->
-	<div class="w-1/6">
-    <Side/>
-	</div>
-	<div class="w-4/6">
-		<slot />
+<div class="w-4/5 mx-auto py-12">
+	<div class="grid grid-cols-5">
+		<nav in:slide={{ axis: 'x', duration: 600 }} class="py-3">
+			<!-- TODO: make into components -->
+			<a
+				href="/dashboard/upload"
+				class="p-3 bg-emerald-500 text-white text-lg font-sans font-bold w-full flex">
+				<p>upload</p>
+			</a>
+			<a
+				href="/dashboard/"
+				class="group p-3 text-neutral-500 border-l-2 border-l-emerald-500 text-lg font-sans font-bold flex gap-3 justify-between">
+				<p class="group-hover:text-neutral-300">messages</p>
+				<p
+					class="group-hover:visible invisible group-hover:-translate-x-8 duration-300 text-emerald-300">
+					←
+				</p>
+			</a>
+			<a
+				href="/dashboard/maintenance"
+				class="group p-3 text-neutral-500 border-l-2 border-l-emerald-500 text-lg font-sans font-bold flex gap-3 justify-between">
+				<p class="group-hover:text-neutral-300">maintenance</p>
+				<p
+					class="group-hover:visible invisible group-hover:-translate-x-8 duration-300 text-emerald-300">
+					←
+				</p>
+			</a>
+		</nav>
+		<div class="col-span-3 py-3">
+			<slot />
+		</div>
+
+		{#if $page.url.pathname === '/dashboard/upload'}
+			<div class="py-3">
+				<p>only <span class="text-rose-500">gif</span> files are allowed</p>
+				<p>maximum file size is <span class="text-rose-500">40 MiB</span></p>
+			</div>
+		{/if}
 	</div>
 </div>
--- src/routes/dashboard/upload/+page.svelte
+++ src/routes/dashboard/upload/+page.svelte
@@ -2,11 +2,11 @@
 	import { PUBLIC_FILE_SIZE_LIMIT } from '$env/static/public';
 
 	let currentFile: File | undefined;
-	let uploadState = 'ready';
+	let uploadState = 'ready to upload';
 	let isDragOver = false;
 	let dragStyle = '';
 
-	$: dragStyle = isDragOver ? 'bg-zinc-500' : 'bg-zinc-900';
+	$: dragStyle = isDragOver ? 'scale-110' : '';
 
 	function eventFileDropped(event: DragEvent) {
 		console.log(event.dataTransfer?.files[0]);
@@ -53,38 +53,33 @@
 	}
 </script>
 
-<div class="py-6 w-2/3 mx-auto">
-	<h2 class="text-2xl pb-3 font-bold">Rules</h2>
-	<!-- info -->
-	<div class="text-justify pb-6">
-		<p>❗ only <span class="text-rose-600">.gif</span> uploads allowed</p>
-		<!-- TODO: add GET endpoint to get the max file size limit from the server -->
-		<p>❗ size limit is {parseInt(PUBLIC_FILE_SIZE_LIMIT) / 1000000} MB</p>
-	</div>
-
+<div class="text-neutral-500 flex flex-col w-4/5 mx-auto">
 	<!-- upload box -->
-	<form class=" flex flex-col gap-6">
-		<div class="mx-auto">{uploadState}</div>
+	<form class=" flex flex-col gap-3">
+		<!-- TODO: uplloadState needs to be a object or something that has the colors and state together -->
+		<div class=" text-emerald-500 py-3 text-lg {isDragOver ? "-translate-y-4  ":""} transition ease-in-out duration-300">{uploadState}</div>
 		<!-- TODO: add colors and so on-->
 		<!-- TOOD: fix weird behaviour with dropzone. maybe remove the whole dropzone thing and make it a regular upload -->
-		<div class="h-60 items-center flex justify-center rounded-sm">
+		<div class="flex justify-center rounded-sm">
 			<label
 				on:drop|preventDefault|stopPropagation={eventFileDropped}
 				on:dragover|preventDefault|stopPropagation={() => {
 					isDragOver = true;
 				}}
-				on:dragend|preventDefault|stopPropagation={() => {isDragOver = false;}}
+				on:dragend|preventDefault|stopPropagation={() => {
+					isDragOver = false;
+				}}
 				on:dragleave={() => {
 					isDragOver = false;
 				}}
 				on:input={filePicked}
 				for="dropzone"
-				class="w-full items-center flex flex-col justify-center h-full {dragStyle} truncate border-dashed border-2 border-emerald-900 hover:border-emerald-700"
+				class="w-full aspect-video border-dashed border-2 transition ease-in-out duration-300 {dragStyle}  hover:border-neutral-500 border-neutral-200 flex items-center justify-center mx-auto "
 			>
 				{#if currentFile}
 					<p class="pointer-events-none">{currentFile.name}</p>
 				{:else}
-					<p class="pointer-events-none">drop file here</p>
+					<p class="pointer-events-none">drag & drop</p>
 				{/if}
 				<input id="dropzone" type="file" class="hidden pointer-events-none" />
 			</label>
@@ -92,7 +87,7 @@
 		<!--TODO: remove button -->
 		<div
 			on:click={uploadFile}
-			class="items-center flex justify-center w-full bg-emerald-900 hover:bg-emerald-700 rounded-sm cursor-pointer"
+			class="{isDragOver ? "translate-y-4  ":""} transition ease-in-out duration-300 items-center flex justify-center w-full bg-neutral-100 hover:bg-neutral-100 hover:-translate-y-1 shadow-md hover:drop-shadow-md cursor-pointer"
 		>
 			<div class="p-4 text-lg font-bold">upload</div>
 		</div>
